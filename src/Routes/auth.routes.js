91df6a624b92d8b9592399ef98f6941c4dc4d6c0
AbhayPartap-Singh <partapabhay613@gmail.com>
2026-02-17 01:08:41 +0530

    first commit

---
+++ src/Routes/auth.routes.js
@@ -0,0 +1,116 @@
+const express = require("express")
+const userModel = require("../model/user.model")
+const crypto = require("crypto")
+const authRouter = express.Router()
+
+authRouter.post("/register", async (req,res)=>{
+    const {username,email,password,bio,ProfileImage}=  req.body
+    
+    /*    const isusernameExsits = await userModel.findOne({username})
+
+    if(isusernameExsits){
+        return res.status(401).json({
+          message:"already in use"
+        })
+    }
+
+    const isEmailexsits = await userModel.findOne({email})
+
+    if(isEmailexsits){
+        return res.status(401).json({
+          message:"already in use"
+        })
+    }
+        */
+    
+    const isUserAlreadyExsists = await userModel.findOne({
+        $or:[
+            {username},
+            {email}
+        ]
+    })
+    if(isUserAlreadyExsists){
+        return res.status(409).json({
+            message:"User already exsists"+(isUserAlreadyExsists ===
+             email?"email Already exsists":"username exsits")
+        })
+    }
+
+const hash = crypto.createHash('md5').update(password).diggest('hex')
+
+const user = userModel.create({
+    username,
+    email,
+    password:hash,
+    bio,
+    ProfileImage
+})
+
+const token = jwt.sign({
+    id:user._id
+},process.env.JWT_SECRET)
+
+res.cookie("jwt_token",token)
+
+res.status(201).json({
+    message:"User registered Successfully",
+    user:{
+        username:(await user).username,
+        email:(await user).email,
+        password:(await user).password,
+        bio:(await user).bio,
+        ProfileImage:(await user).ProfileImage
+
+    }
+})
+
+
+
+
+})
+authRouter.post("/login", async (req,res)=>{
+    const{username,email,password} = req.body
+
+    const user = await userModel.findOne({
+        $or:[
+            {
+                username:username
+            },
+            {
+                email:email
+            }
+        ]
+    })
+    if(!user){
+        return res.status(404).json({
+            message:"user not found"
+        })
+    }
+    const hash = crypto.createHash('md5').update(password).diggest('hex')
+
+    const ispasswordValid = hash == user.password
+   if(!ispasswordValid){
+    return res.status(401).json({
+        massage:"password is invalid"
+    })
+   }
+   const token = jwt.sign({
+    id:user._id
+     },process.env.JWT_SECRET,
+     {expiresIn:'1d'})
+     res.cookie ("token",token)
+
+     res.status(200).json({
+        message:"user logged in",
+        user:{
+            username:user.username,
+            email:user.email,
+            bio:user.bio,
+            password:user.password,
+            ProfileImage:user.ProfileImage
+        }
+     })
+
+})
+
+module.exports = authRouter
---
+++ src/model/user.model.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose")
+
+const userSchema = new mongoose.Schema({
+    username:{
+        type:String,
+        unique:[true,"it al;ready exsits"],
+        required:[true,"it is required"]
+    },
+    email:{
+        type:String,
+        unique:[true,"it al;ready exsits"],
+        required:[true,"it is required"]
+    },
+    password:{
+        type:String,
+        required:[true,"it is required"]
+    },
+    bio:String,
+    ProfileImage:{
+        type:String,
+        default:"https://ik.imagekit.io/uxl1bm5cd/7494a27824f83290d51d65b731b0e716.jpg"
+    }
+     
+    
+})
+
+const userModel = mongoose.model('users',userSchema)
+
+module.exports = userModel
